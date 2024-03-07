0444b4385d761bc9e3fdcfb8d831617894d5607d
Guilherme-Santos-C <guilherme44.santos.costa@gmail.com>
2024-03-07 06:45:56 -0300

    bibliografia pagina feita

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const carousel = document.querySelector(".carrosel-menu-container");
-  const prevButton = document.querySelector(".btn1-carrosel");
-  const nextButton = document.querySelector(".btn2-carrosel");
+    const carousel = document.querySelector(".carrosel-menu-container");
+    const prevButton = document.querySelector(".btn1-carrosel");
+    const nextButton = document.querySelector(".btn2-carrosel");
 
-  const cardWidth = document.querySelector(".card-carrosel").offsetWidth; // Largura do card
-  const cardMargin = 20; // Espaçamento entre os cards
+    const cardWidth = document.querySelector(".card-carrosel").offsetWidth; // Largura do card
+    const cardMargin = 20; // Espaçamento entre os cards
 
-  let position = 0;
-  const wrapperWidth = document.querySelector(".section2").offsetWidth;
-  const numCards = document.querySelectorAll(".card-carrosel").length;
-  const totalWidth = (cardWidth + cardMargin) * numCards - cardMargin; // Largura total dos cards
+    let position = 0;
+    const wrapperWidth = document.querySelector(".section2").offsetWidth;
+    const numCards = document.querySelectorAll(".card-carrosel").length;
+    const totalWidth = (cardWidth + cardMargin) * numCards - cardMargin; // Largura total dos cards
 
-  function moveCarousel() {
-      carousel.style.transform = `translateX(${position}px)`;
-  }
+    function moveCarousel() {
+        carousel.style.transform = `translateX(${position}px)`;
+    }
 
-  nextButton.addEventListener("click", function() {
+    nextButton.addEventListener("click", function() {
       const maxPosition = wrapperWidth - totalWidth - 1100; // Posição máxima com margem de segurança
 
-      if (position > maxPosition) {
-          position -= cardWidth + cardMargin; // Mover para o próximo card
-          if (position < maxPosition) {
+        if (position > maxPosition) {
+            position -= cardWidth + cardMargin; // Mover para o próximo card
+            if (position < maxPosition) {
               position = maxPosition; // Ajustar para a posição máxima se necessário
-          }
-          moveCarousel();
-      }
-  });
+            }
+            moveCarousel();
+        }
+    });
 
-  prevButton.addEventListener("click", function() {
-      if (position < 0) {
-          position += cardWidth + cardMargin; // Mover para o card anterior
-          if (position > 0) {
+    prevButton.addEventListener("click", function() {
+        if (position < 0) {
+            position += cardWidth + cardMargin; // Mover para o card anterior
+            if (position > 0) {
               position = 0; // Ajustar para a posição inicial se necessário
-          }
-          moveCarousel();
-      }
-  });
+            }
+            moveCarousel();
+        }
+    });
 });
+
+const botao_section1 = document.querySelector(".btn-section1")
+botao_section1.addEventListener("click", () => {
+    window.scroll(0, 550)
+})
+
+console.log(botao_section1)
